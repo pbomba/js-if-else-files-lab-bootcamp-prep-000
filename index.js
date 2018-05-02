78ba570f1ffaa451082dca5c6f51f2dfb3399169
Paul Bomba` <paul@paulbomba.com>
2018-05-02 19:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var name = "Joe"
 var height = "74"
 
 if (name == "Susan") {
-  
-  
-  
-  
+  console.log("Correct");
+} else {
+  console.log("expected name to be Susan");
 }
+
 // Don't worry about this
 module.exports = { name, height
 }
